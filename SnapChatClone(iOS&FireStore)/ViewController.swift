16cd2497a4199efc7bfcdacaa314244244f656ca
radhimighri <radhi.mighri@polytechnicien.tn>
2020-10-17 13:36:11 -0700

    Initial Commit

---
+++ SnapChatClone(iOS&FireStore)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapChatClone(iOS&FireStore)
+//
+//  Created by Radhi Mighri on 17/10/2020.
+//  Copyright © 2020 Radhi Mighri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
